a07669d97f9fccfe4e5e30c158ed1c5a995a50db
mThayyabSmiligence <mohammedthayyab.a.r@smiligence.org>
2025-10-07 10:50:58 +0530

    first commit, 1. addes tailwind stepup and router setup

---
+++ src/pages/Index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+
+export default function Index() {
+  return (
+    <div>
+        <Navbar/>
+        <Hero/>
+        hkdfhalkdsjh
+    </div>
+  )
+}
